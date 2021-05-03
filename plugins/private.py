5ef4eafb927610185eeacdf3654149c05c4d50c8
Dash Eclipse <dashezup@disroot.org>
2021-05-03 02:00:56 +0000

    feat: plugins.private: allow users to check the count of their saved whispers and delete them

--- plugins/private.py
+++ plugins/private.py
@@ -15,10 +15,33 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from pyrogram import Client, filters
+from datetime import datetime
+
+from pyrogram import Client, filters, emoji
 from pyrogram.types import (Message,
                             InlineKeyboardMarkup, InlineKeyboardButton,
                             CallbackQuery)
+
+from data import whispers
+
+LEARN_TEXT = (
+    "This bot works only in inline mode, a example use would be like "
+    "this:\n\n"
+    "- Write a whisper to @username\n"
+    "`@ezWhisperBot @username some text here`\n\n"
+    "- Whisper to the first one who open it (can also be used in PM)\n"
+    "`@ezWhisperBot some text here`"
+)
+LEARN_REPLY_MARKUP = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "Next",
+                callback_data="learn_next"
+            )
+        ]
+    ]
+)
 
 DEFAULT_TEXT = (
     "This bot allows you to send whisper messages, "
@@ -38,6 +61,12 @@ DEFAULT_REPLY_MARKUP = InlineKeyboardMarkup(
                 "Try in This Chat",
                 switch_inline_query_current_chat=""
             )
+        ],
+        [
+            InlineKeyboardButton(
+                "My Whispers",
+                callback_data="list_whispers"
+            )
         ]
     ]
 )
@@ -49,24 +78,8 @@ DEFAULT_REPLY_MARKUP = InlineKeyboardMarkup(
                    & filters.command("start"))
 async def command_start(_, m: Message):
     if len(m.command) == 2 and m.command[1] == "learn":
-        text_start = (
-            "This bot works only in inline mode, a example use would be like "
-            "this:\n\n"
-            "- Write a whisper to @username\n"
-            "`@ezWhisperBot @username some text here`\n\n"
-            "- Whisper to the first one who open it (can also be used in PM)\n"
-            "`@ezWhisperBot some text here`"
-        )
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Next",
-                        callback_data="learn_next"
-                    )
-                ]
-            ]
-        )
+        text_start = LEARN_TEXT
+        reply_markup = LEARN_REPLY_MARKUP
     else:
         text_start = DEFAULT_TEXT
         reply_markup = DEFAULT_REPLY_MARKUP
@@ -77,11 +90,63 @@ async def command_start(_, m: Message):
     )
 
 
-@Client.on_callback_query(filters.regex("^learn_next$"))
-async def back_to_inline(_, cq: CallbackQuery):
+@Client.on_callback_query(filters.regex("^(learn_next|start)$"))
+async def show_main_page(_, cq: CallbackQuery):
     await cq.edit_message_text(
         text=DEFAULT_TEXT,
         disable_web_page_preview=True,
         reply_markup=DEFAULT_REPLY_MARKUP
     )
-    await cq.answer("Now you can back to inline mode")
+    if cq.data == "learn_next":
+        await cq.answer("Now you can back to inline mode")
+
+
+@Client.on_callback_query(filters.regex("^list_whispers$"))
+async def list_whispers(_, cq: CallbackQuery):
+    user_id = cq.from_user.id
+    user_whispers = [
+        i for i in whispers.values() if i['sender_uid'] == user_id
+    ]
+    if len(user_whispers) == 0:
+        text = "You don't have any whispers"
+    else:
+        text = f"You have **{len(user_whispers)}** whispers"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    f"{emoji.WASTEBASKET}  Delete My Whispers",
+                    callback_data="delete_my_whispers"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    f"{emoji.BACK_ARROW}  Back to Main Page",
+                    callback_data="start"
+                )
+            ]
+        ]
+    )
+    await cq.edit_message_text(
+        text=text,
+        reply_markup=reply_markup
+    )
+
+
+@Client.on_callback_query(filters.regex("delete_my_whispers"))
+async def delete_my_whispers(_, cq: CallbackQuery):
+    user_id = cq.from_user.id
+    deleted_whispers = [
+        whispers.pop(k)
+        for k, v in list(whispers.items())
+        if v['sender_uid'] == user_id
+    ]
+    if len(deleted_whispers) == 0:
+        await cq.answer("You don't have any whispers")
+    else:
+        await cq.answer(f"Removed {len(deleted_whispers)} whispers")
+        utcnow = datetime.utcnow().strftime('%F %T')
+        await cq.edit_message_text(
+            f"Your whispers has been removed at `{utcnow}`",
+            reply_markup=cq.message.reply_markup
+        )
